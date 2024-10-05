ef4a914593bafae8cd9ef3a60eaf71e5619c3dad
Siabel <128214383+Siabel@users.noreply.github.com>
2024-10-06 04:47:17 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt = 1;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(cnt < 10){
+                cout << cnt;
+                cnt++;
+            }
+            else{
+                cnt = 1;
+                cout << cnt;
+                cnt++;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
