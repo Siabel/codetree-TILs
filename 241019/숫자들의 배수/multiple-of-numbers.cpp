30341ce2359cbecb78cf5bdf1dd14111afd90e9f
Siabel <128214383+Siabel@users.noreply.github.com>
2024-10-19 04:18:35 +0900

    Title: 숫자들의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241019/숫자들의 배수/multiple-of-numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int cnt = 0;
+    int arr[11] = {};
+    arr[0] = n;
+
+    for(int i = 1; i < 11; i++){
+        if(i % 5 == 0){
+            cnt++;
+        }
+        if(cnt == 2)
+            break;
+
+        arr[i] = n * (i + 1);
+    }
+    
+    for(int i = 0; i < 11; i++){
+        if (arr[i] != 0)
+            cout << arr[i] << " ";
+    }
+
+    return 0;
+}
