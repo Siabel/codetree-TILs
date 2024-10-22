76edc1c42ce976df28820267320a4e220bcf12a4
Siabel <128214383+Siabel@users.noreply.github.com>
2024-10-23 03:46:20 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241023/1-9 개수 세기/count-one-to-nine.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int arr[100];
+    int count_arr[10] = {};
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++){
+        count_arr[arr[i]]++;
+    }
+
+    for(int i = 1; i < 10; i++){
+        cout << count_arr[i] << "\n";
+    }
+    return 0;
+}
