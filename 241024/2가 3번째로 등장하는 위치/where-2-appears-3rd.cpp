2429020c03079d948c78dfac217e22cf718fd086
Siabel <128214383+Siabel@users.noreply.github.com>
2024-10-24 12:42:12 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241024/2가 3번째로 등장하는 위치/where-2-appears-3rd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int idx = -1;
+    int cnt = 0;
+    int arr[100];
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+
+        if(arr[i] == 2)
+            cnt++;
+        if(cnt == 3)
+            idx = i;
+    }
+
+    cout << idx - 1;
+    return 0;
+}
