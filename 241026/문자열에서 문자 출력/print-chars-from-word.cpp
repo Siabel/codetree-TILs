1c82e21c2362090a3bef851ab54a734121c6285c
Siabel <128214383+Siabel@users.noreply.github.com>
2024-10-26 15:20:14 +0900

    Title: 문자열에서 문자 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241026/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+
+    for(int i = 0; arr[i] != "\0"; i++){
+        cout << arr[i] << endl;
+    }
+    
+    return 0;
+}
