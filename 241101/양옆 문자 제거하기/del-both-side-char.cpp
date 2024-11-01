fd0d35a179ca82e615eed27350cf8633f70d635e
Siabel <128214383+Siabel@users.noreply.github.com>
2024-11-01 15:59:14 +0900

    Title: 양옆 문자 제거하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241101/양옆 문자 제거하기/del-both-side-char.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string str;
+    cin >> str;
+
+    str.erase(0,2);
+    str = str.substr(0, str.length() - 4)
+    cout << str;
+    
+    return 0;
+}
