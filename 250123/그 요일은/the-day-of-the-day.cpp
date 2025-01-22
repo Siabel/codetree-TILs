8984bd3926cc918b7b747abf0f1648e1bed08f25
Siabel <128214383+Siabel@users.noreply.github.com>
2025-01-23 08:53:42 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250123/그 요일은/the-day-of-the-day.cpp
+++ 250123/그 요일은/the-day-of-the-day.cpp
@@ -1,50 +1,32 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int m1, m2, d1, d2;
-string A;
-string yoil[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-
-int CalDate(int month, int day){
-    int num_of_days[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    
-    int total = 0;
-    for(int i = 1; i < month; i++){
-        total += num_of_days[i];
-    }
-
-    total += day;
-
-    return total;
+int mon(int a){
+    if(a==2) return 29;
+    if(a==1 or a==3 or a==5 or a==7 or a==8 or a==10 or a==12) return 31;
+    return 30;
 }
 
 int main() {
-    cin >> m1 >> d1 >> m2 >> d2;
-    cin >> A;
-
-    // Write your code here!
-    int idx = -1;
-    for(int i = 0; i < 7; i++){
-        if(yoil[i] == A){
-            idx = i;
-            break;
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    int day =0;
+    int cnt =0;
+    string days[7] = {"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    string date;
+    cin>>date;
+    while(!(a==c and b==d)){
+        day++;
+        b++;
+        if(b>mon(a)){
+            a++;
+            b=1;
         }
-    }
-    
-    int start = CalDate(m1, d1);
-    int end = CalDate(m2, d2);
-    int start_week = (start - 1) % 7;
-
-    int cnt = 0;
-    for (int i = start; i <= end; i++) {
-        int cur_week = (start_week + (i - start)) % 7;
-        if (cur_week == idx) {
+        if(date == days[day%7]){
             cnt++;
         }
     }
-
-    cout << cnt;
-
+    cout<<cnt;
     return 0;
 }
