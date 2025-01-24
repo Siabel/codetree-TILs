b7007dc895f0a82da6a3453cbdbb05580d1bdcd1
Siabel <128214383+Siabel@users.noreply.github.com>
2025-01-24 14:08:30 +0900

    Title: 최대로 겹치는 구간, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 250124/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x1[100], x2[100];
+int res[100] = {};
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+
+    // Write your code here!
+    for(int i = 0; i < n; i++){
+        for(int j = x1[i]; j <= x2[i]; j++){
+            res[j]++;
+        }
+    }
+
+    int max = 0;
+    for(int i = 0; i < 100; i++){
+        if(max < res[i])
+            max = res[i];
+    }
+
+    cout << max;
+
+    return 0;
+}
