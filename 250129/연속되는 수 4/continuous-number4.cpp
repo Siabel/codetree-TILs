3cd726cc7c40c217c4069af2403c0e108b3b6541
Siabel <128214383+Siabel@users.noreply.github.com>
2025-01-29 23:32:07 +0900

    Title: 연속되는 수 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250129/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+int arr[1000];
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    // Write your code here!
+    int cnt = 0;
+    int res = 0;
+
+    for (int i = 0; i < N; i++) {
+        if(i == 0 || arr[i] > arr[i - 1]){
+            cnt++;
+            if(res < cnt)
+                res = cnt;
+        }
+        else{
+            if(res < cnt)
+                res = cnt;
+            cnt = 1;
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
