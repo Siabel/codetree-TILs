9d2af24dc9d30424b6396a3bb1ee19773e9092fc
Siabel <128214383+Siabel@users.noreply.github.com>
2025-01-29 23:22:50 +0900

    Title: 연속되는 수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250129/연속되는 수 2/continuous-number2.cpp
+++ 250129/연속되는 수 2/continuous-number2.cpp
@@ -17,7 +17,8 @@ int main() {
     for(int i = 0; i < N; i++){
         if(i == 0 || arr[i] == arr[i - 1]){
             cnt++;
-            res = cnt;
+            if(res < cnt)
+                res = cnt;
         }
         else{
             if(res < cnt)
