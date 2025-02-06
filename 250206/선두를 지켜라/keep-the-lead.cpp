0d753da845e0d6f66e60d5d67d4d6762b197e339
Siabel <128214383+Siabel@users.noreply.github.com>
2025-02-06 23:11:25 +0900

    Title: 선두를 지켜라, Time: 8ms, Memory: 2MB, Status: Passed - CodeTree

--- 250206/선두를 지켜라/keep-the-lead.cpp
+++ 250206/선두를 지켜라/keep-the-lead.cpp
@@ -5,58 +5,67 @@ using namespace std;
 int N, M;
 int v1[1000], t1[1000];
 int v2[1000], t2[1000];
+int res1[1000000] = {};
+int res2[1000000] = {};
+
+int distance1 = 0;
+int distance2 = 0;
+
+int cnt1 = 0, cnt2 = 0;
+int change = 0;
+int first = 0;
 
 int main() {
     cin >> N >> M;
 
     for (int i = 0; i < N; i++) cin >> v1[i] >> t1[i];
+
     for (int i = 0; i < M; i++) cin >> v2[i] >> t2[i];
 
-    int pos1 = 0, pos2 = 0;
-    int idx1 = 0, idx2 = 0;
-    int time1 = 0, time2 = 0;
-    int first = 0, change = 0;
-
-    for (int t = 0; ; t++) {
-        // 현재 시간이 끝났다면 다음 속도로 이동
-        if (idx1 < N && time1 == t1[idx1]) {
-            time1 = 0;
-            idx1++;
+    // Write your code here!
+    for(int i = 0; i < N; i++){
+        while(t1[i]){
+            distance1 += v1[i];
+            res1[cnt1++] = distance1;
+            t1[i]--;
         }
-        if (idx2 < M && time2 == t2[idx2]) {
-            time2 = 0;
-            idx2++;
-        }
+    }
 
-        // 이동 거리 업데이트
-        if (idx1 < N) {
-            pos1 += v1[idx1];
-            time1++;
-        }
-        if (idx2 < M) {
-            pos2 += v2[idx2];
-            time2++;
-        }
+    for(int i = 0; i < M; i++){
+        distance2 += v2[i];
 
-        // 종료 조건: 더 이상 이동할 수 없을 때
-        if (idx1 >= N && idx2 >= M) break;
+        while(1){
+            res2[cnt2++] = distance2;
+            t2[i]--;
+            if(t2[i] == 0){
+                break;
+            }
+            distance2 += v2[i];
+        }
+    }
+    for(int i = 0; i < 1000000; i++){
+        // cout << res1[i] << " " << res2[i]<< "\n";
+        if(res1[i] == 0 || res2[i] == 0)
+            break;
 
-        // 1등 판별
-        if (t == 0) {
-            if (pos1 > pos2) first = 1;
-            else if (pos1 < pos2) first = 2;
-        } else {
-            if (pos1 > pos2 && first == 2) {
+        if(first == 0){
+            if(res1[i] > res2[i])
                 first = 1;
-                change++;
-            }
-            else if (pos1 < pos2 && first == 1) {
+            else if(res1[i] < res2[i])
                 first = 2;
-                change++;
-            }
+        }
+
+        if(res1[i] > res2[i] && first == 2){
+            first = 1;
+            change++;
+        }
+        else if(res1[i] < res2[i] && first == 1){
+            first = 2;
+            change++;
         }
     }
 
     cout << change;
+
     return 0;
 }
