8082b895aa911f3c0e13740436be2a99e574bff3
Siabel <128214383+Siabel@users.noreply.github.com>
2026-07-08 23:42:22 +0900

    [Wrong Answer] 비교 연산 (60ms, 10MB)

---
+++ javascript/trail0/비교 연산/comparison-operator.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim().split(" "));
+
+let A = input[0];
+let B = input[1];
+
+if(A >= B){
+    console.log(1);
+}
+
+if(A >= B){
+    console.log(1);
+}
+
+if(A >= B){
+    console.log(1);
+}
