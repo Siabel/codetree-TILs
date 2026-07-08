6beb2a1a373e5426c34aa8e860fa5ed4ecaef07f
Siabel <128214383+Siabel@users.noreply.github.com>
2026-07-09 00:07:54 +0900

    [Passed] 물의 상태 (62ms, 10MB)

---
+++ javascript/trail0/물의 상태/state-of-water.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim().split(" ");
+
+if(n < 0){
+    console.log("ice");
+} else if (n >= 100) {
+    console.log("vapor");
+} else {
+    console.log("water");
+}
