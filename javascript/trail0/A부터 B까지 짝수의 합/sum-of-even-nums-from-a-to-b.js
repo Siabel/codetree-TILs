f0b90e860f9cff1cb0da04e1d682123a6bdc7804
Siabel <128214383+Siabel@users.noreply.github.com>
2026-07-14 22:27:38 +0900

    [Passed] A부터 B까지 짝수의 합 (72ms, 10MB)

---
+++ javascript/trail0/A부터 B까지 짝수의 합/sum-of-even-nums-from-a-to-b.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let total = 0;
+
+for(let i = a; i <= b; i++){
+    if(i % 2 == 0){
+        total += i;
+    }
+}
+
+console.log(total)
