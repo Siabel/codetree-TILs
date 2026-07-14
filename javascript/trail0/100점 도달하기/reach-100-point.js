537a67f335e1ebe27e319bf3143548b3e31fde62
Siabel <128214383+Siabel@users.noreply.github.com>
2026-07-14 18:00:01 +0900

    [Wrong Answer] 100점 도달하기 (65ms, 10MB)

---
+++ javascript/trail0/100점 도달하기/reach-100-point.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let N = Number(fs.readFileSync(0).toString().trim());
+
+let res = [];
+
+for (let i = N; i <= 100; i++) {
+    if (i >= 90) {
+        res += "A ";
+    } else if (i >= 80) {
+        res += "B ";
+    } else if (i >= 70) {
+        res += "C ";
+    } else if (i >= 60) {
+        res += "D ";
+    } else {
+        res += "E ";
+    }
+}
+
+console.log(res);
